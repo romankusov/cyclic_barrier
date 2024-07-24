e319df25c16507e75dd25611559b626cc15741ba
Roman Kusov <rkusoff@gmail.com>
2024-07-24 17:32:43 +0400

    Модуль 3. Топик 20. CyclicBarrier - refactoring 2

--- src/main/java/org/example/ComplexTaskExecutor.java
+++ src/main/java/org/example/ComplexTaskExecutor.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ComplexTaskExecutor {
     private final CyclicBarrier barrier;
     private final List<ComplexTask> tasks;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public ComplexTaskExecutor(int numberOfTasks) {
         this.tasks = new ArrayList<>();
@@ -22,15 +24,21 @@ public class ComplexTaskExecutor {
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfTasks);
         try {
             for (int i = 0; i < numberOfTasks; i++) {
-                tasks.add(new ComplexTask(i, barrier));
-                executorService.submit(tasks.get(i));
+                lock.lock();
+                try {
+                    tasks.add(new ComplexTask(i, barrier));
+                    executorService.submit(tasks.get(i));
+                } finally {
+                    lock.unlock();
+                }
             }
+            System.out.println("List size: " + tasks.size());
         } finally {
             executorService.shutdown();
         }
     }
 
-    private void mergeResults() {
+    private synchronized void mergeResults() {
         int totalSum = tasks.stream().mapToInt(ComplexTask::getResult).sum();
         System.out.println("Total sum of results: " + totalSum);
     }
